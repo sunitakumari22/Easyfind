854e76fadebe7e773de649571373f194b1b94336
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-05 16:00:58 +0530

    updated  hotels.component

--- easyfind/src/app/components/hotels/hotels.component.ts
+++ easyfind/src/app/components/hotels/hotels.component.ts
@@ -3,10 +3,13 @@ import { HotelService } from '../../service/hotel.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { SearchLocationService } from '../../service/search-location.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hotels',
-  imports: [CommonModule],
+  imports: [CommonModule,ToolbarModule,FormsModule],
   providers: [MessageService],
   templateUrl: './hotels.component.html',
   styleUrl: './hotels.component.scss'
@@ -14,21 +17,34 @@ import { CommonModule } from '@angular/common';
 export class HotelsComponent {
   hotels: any[] = [];
   filteredHotels: any[] = [];
+  location:string=''
 
   constructor(private hotelService: HotelService,
     private router: Router,
     private route: ActivatedRoute,
     private messageService: MessageService,
+    private searchLocation: SearchLocationService
   ) { }
 
   ngOnInit() {
+    this.getLocation();
+    this.getHotel();
+    
+  }
+  getHotel(){
     this.hotelService.getHotels().subscribe(response => {
       this.hotels = response;
       console.log(this.hotels);
 
       this.filteredHotels = [...this.hotels];
     });
+
   }
+  getLocation(){
+    this.searchLocation.getLocation().subscribe(data => {
+        this.location=data.city;
+
+    })}
 }
 
 
