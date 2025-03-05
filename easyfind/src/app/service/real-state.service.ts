6d35c09ebbdea24b7c7240cc43224abeec02e9b7
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-05 15:23:33 +0530

    Created service for Real State component

---
+++ easyfind/src/app/service/real-state.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RealStateService {
+
+  
+    constructor(private http: HttpClient) { }
+    
+      getRealState(): Observable<any> {
+        return this.http.get('http://localhost:3000/RealState'); 
+      }
+    }
