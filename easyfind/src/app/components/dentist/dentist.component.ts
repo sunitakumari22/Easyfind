0fb6145123c96910f6b04e701d2a23aa049d2904
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-19 14:41:47 +0530

    Created Dentist component

---
+++ easyfind/src/app/components/dentist/dentist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dentist',
+  imports: [],
+  templateUrl: './dentist.component.html',
+  styleUrl: './dentist.component.scss'
+})
+export class DentistComponent {
+
+}
