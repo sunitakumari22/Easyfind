5cc281cee1264df8c983faebe358c58cd1f63ffd
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-19 14:42:02 +0530

    Created Gym component

---
+++ easyfind/src/app/components/gym/gym.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gym',
+  imports: [],
+  templateUrl: './gym.component.html',
+  styleUrl: './gym.component.scss'
+})
+export class GymComponent {
+
+}
