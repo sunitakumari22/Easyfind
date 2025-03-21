03965a3e0f2414c5c59e2348654773d0245d89ec
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-21 12:28:46 +0530

    Added restaurants component style changes and Api integration

--- easyfind/src/app/components/restaurants/restaurants.component.ts
+++ easyfind/src/app/components/restaurants/restaurants.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RestaurantsService } from '../../service/restaurants.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-restaurants',
-  imports: [],
+  imports: [CommonModule],
+  providers:[MessageService],
   templateUrl: './restaurants.component.html',
   styleUrl: './restaurants.component.scss'
 })
 export class RestaurantsComponent {
+   restaurants: any[] = [];
+       filteredRestaurants: any[] = [];
+      
+        constructor(private restaurantsService: RestaurantsService,
+          private router: Router,
+          private route: ActivatedRoute,
+          private messageService: MessageService,
+        ) {}
+      
+        ngOnInit() {
+            this.restaurantsService.getRestaurants().subscribe(response => {
+              this.restaurants = response;
+              
+              this.filteredRestaurants = [...this.restaurants];
+            });
+          }
+        
 
 }
