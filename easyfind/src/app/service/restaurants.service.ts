d4f23a63b322f647bd096f26804e93dd096005d5
SunitaKumari <sunita.kumari@sbu.ac.in>
2025-03-21 12:29:08 +0530

    Created service file for restaurants component

--- easyfind/src/app/service/restaurants.service.ts
+++ easyfind/src/app/service/restaurants.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestaurantsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+      
+        getRestaurants(): Observable<any> {
+          return this.http.get('http://localhost:5000/api/restroList'); 
+        }
 }
