f66a626cbf841a705e4df58516d419115c226c6f
SunitaKumari <172397566+sunitakumari22@users.noreply.github.com>
2025-04-03 11:51:11 +0530

    Added toast msg in login component

--- easyfind/src/app/components/login/login.component.ts
+++ easyfind/src/app/components/login/login.component.ts
@@ -4,10 +4,13 @@ import { AuthService } from '../../service/auth.service';
 import { NgModule } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, Validators,ReactiveFormsModule  } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,ReactiveFormsModule,CommonModule],
+  imports: [FormsModule,ReactiveFormsModule,CommonModule,ToastModule],
+  providers:[MessageService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -15,7 +18,9 @@ export class LoginComponent {
   loginForm: FormGroup;
   users: any[] = [];
   errorMessage: string = '';
-  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) {
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder,
+    private messageService: MessageService,
+  ) {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
       password: ['', Validators.required]
@@ -30,6 +35,12 @@ export class LoginComponent {
           localStorage.setItem('loggedInUser', JSON.stringify(success));
           this.router.navigate(['/']);
         } else {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'Invalid email or password',
+            life: 3000
+        });
           this.errorMessage = 'Invalid email or password';
         }
       },
